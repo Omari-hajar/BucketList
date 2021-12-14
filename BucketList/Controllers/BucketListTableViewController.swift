381b3dcacc9dfaedf964895e41feaad8a4e85624
Omari-hajar <89646158+Omari-hajar@users.noreply.github.com>
2021-12-14 12:04:15 +0300

    edit

--- BucketList/Controllers/BucketListTableViewController.swift
+++ BucketList/Controllers/BucketListTableViewController.swift
@@ -74,7 +74,7 @@ class BucketListTableViewController: UITableViewController, AddItemTableViewDele
 //
 //    }
     
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination as! UINavigationController
         if let addVC = (vc.topViewController as? AddItemTableViewController) {
